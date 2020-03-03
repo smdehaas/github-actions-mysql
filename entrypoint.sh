1a47fd505c1ac26a7c7e1eba685d7568cb01d4b7
cweinberger <christian.weinberger@me.com>
2020-03-03 12:58:52 +0100

    Fix docker run command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cmd="docker run"
+cmd="docker run --rm"
 
 if [ -n "$INPUT_ROOTPASSWORD" ]; then
   cmd="$cmd -e MYSQL_ROOT_PASSWORD=$INPUT_ROOTPASSWORD"
@@ -19,13 +19,13 @@ if [ -n "$INPUT_DATABASE" ]; then
 fi
 
 cmd="$cmd -d -p $INPUT_HOSTPORT:$INPUT_CONTAINERPORT"
-cmd="$cmd --hostname=$INPUT_HOSTNAME"
+#cmd="$cmd --hostname=$INPUT_HOSTNAME"
+#cmd="$cmd --name=mysql"
 cmd="$cmd mysql:$INPUT_VERSION"
 cmd="$cmd --port=$INPUT_CONTAINERPORT"
 cmd="$cmd --character-set-server=$INPUT_CHARACTERSET"
 cmd="$cmd --collation-server=$INPUT_COLLATION"
 cmd="$cmd --sql-mode=NO_ENGINE_SUBSTITUTION"
-cmd="$cmd --name=mysql"
 
 echo "CMD: $cmd"
 
