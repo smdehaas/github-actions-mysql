8bf845c48504d737517ad0dbb871bfa3918e4ad0
Sander de Haas <sander@webbio.nl>
2021-04-08 09:40:12 +0200

    Allow empty password

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ cmd="docker run --rm"
 
 if [ -n "$INPUT_ROOTPASSWORD" ]; then
   cmd="$cmd -e MYSQL_ROOT_PASSWORD=$INPUT_ROOTPASSWORD"
-elif [ -n "$INPUT_USER" ] && [ -z "$INPUT_PASSWORD" ]; then
-  echo "If you provide a `user` then the `password` should not be empty. Exiting...."
-  exit 1
 elif [ -n "$INPUT_USER" ] && [ -n "$INPUT_PASSWORD" ]; then
   cmd="$cmd -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER=$INPUT_USER -e MYSQL_PASSWORD=$INPUT_PASSWORD"
 else
@@ -26,7 +23,8 @@ cmd="$cmd --port=$INPUT_CONTAINERPORT"
 cmd="$cmd --character-set-server=$INPUT_CHARACTERSET"
 cmd="$cmd --collation-server=$INPUT_COLLATION"
 cmd="$cmd --sql-mode=NO_ENGINE_SUBSTITUTION"
-
+cmd="$cmd --default-authentication-plugin=mysql_native_password"
+cmd="$cmd -e ALLOW_EMPTY_PASSWORD=yes"
 echo "CMD: $cmd"
 
 sh -c "$cmd"
