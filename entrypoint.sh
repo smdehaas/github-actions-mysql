46284ab8b01d09c957e6cbe738c27f555f82fb1c
Sander de Haas <sander@webbio.nl>
2021-04-08 09:42:52 +0200

    remove user related stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,6 @@ cmd="docker run --rm"
 
 if [ -n "$INPUT_ROOTPASSWORD" ]; then
   cmd="$cmd -e MYSQL_ROOT_PASSWORD=$INPUT_ROOTPASSWORD"
-elif [ -n "$INPUT_USER" ] && [ -n "$INPUT_PASSWORD" ]; then
-  cmd="$cmd -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER=$INPUT_USER -e MYSQL_PASSWORD=$INPUT_PASSWORD"
-else
-  echo "No credentials for root or user have been provided. Exiting...."
-  exit 1
 fi
 
 if [ -n "$INPUT_DATABASE" ]; then
